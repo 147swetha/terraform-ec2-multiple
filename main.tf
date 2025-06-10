e6ff4f8e1fb8a585f16798e95905e5d63c8d70ce
147swetha <swetharvind147@gmail.com>
2025-06-10 13:07:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  count         = var.instance_count
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "AutoEC2-${count.index + 1}"
+  }
+}
