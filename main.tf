195dd482eccf416f44c9b1de4484cb8b0950ed2b
147swetha <swetharvind147@gmail.com>
2025-06-11 11:26:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   count         = var.instance_count
   ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2
   instance_type = var.instance_type
-  key_name      = "Devops-key"
+  key_name      = "ansible-key"
 
   tags = {
     Name = "AutoEC2-${count.index + 1}"
